7a645bf13ae74d5619bae547b75a5f99d35df04e
Kirito <kirito@example.com>
2026-04-26 12:14:55 +0800

    Add p115strgmsub plugin with JuYing support, restructure to plugins.v2/p115strgmsub/

--- plugins.v2/juyingsearch/clients/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-客户端模块
-包含聚影 API 客户端
-"""
-from .juying_client import JuyingClient
-
-__all__ = ["JuyingClient"]
--- plugins.v2/juyingsearch/handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-处理器模块
-包含搜索、API 等处理逻辑
-"""
-from .search import SearchHandler
-from .api import ApiHandler
-
-__all__ = ["SearchHandler", "ApiHandler"]
